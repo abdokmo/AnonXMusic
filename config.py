42bada8b8dc2fa390725f1215def00892be6c885
abdokmo <156862349+abdokmo@users.noreply.github.com>
2024-01-26 18:09:47 +0200

    Update config.py
    Signed-off-by: abdokmo <156862349+abdokmo@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/sorcefraon",
+    "https://github.com/abdokmo/AnonXMusic.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
