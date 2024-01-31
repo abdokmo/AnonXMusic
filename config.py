c9cdeb764c7c80c4ef452404c9dde5df86ae8056
abdokmo <156862349+abdokmo@users.noreply.github.com>
2024-02-01 01:31:40 +0200

    Update config.py
    Signed-off-by: abdokmo <156862349+abdokmo@users.noreply.github.com>

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/e2120ef2b55fbd6e7b00e.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/f68bd01bad069f7be552e.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
